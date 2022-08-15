208c5dc4952a74f6018f414db93074d135a6fc9f
Ben Tucker <bwtucker4@gmail.com>
2022-08-15 13:35:12 -0700

    Change shop to functional component in order to implement upcoming product filter

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,62 +1,56 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import allProducts from "../products/allProducts";
 import categories from "../products/categories";
 import "../styles/Shop.css";
 import "../styles/App.css";
 
-class Shop extends Component {
-    constructor(props) {
-        super(props);
-    }
+const Shop = () => {
+    const [filteredProducts, setFilteredProducts] = useState([]);
 
-    componentDidMount() {
+    useEffect(() => {
         document.querySelector(".app-title").style.color = "black";
         document.getElementById("search-icon").style.fill = "black";
         document.getElementById("cart-icon").style.fill = "black";
-    }
 
-    componentWillUnmount() {
-        document.querySelector(".app-title").style.color = "white";
-        document.getElementById("search-icon").style.fill = "white";
-        document.getElementById("cart-icon").style.fill = "white";
-    }
+        return () => {
+            document.querySelector(".app-title").style.color = "white";
+            document.getElementById("search-icon").style.fill = "white";
+            document.getElementById("cart-icon").style.fill = "white";
+        };
+    });
 
-    render() {
-        return (
-            <div className="shop-container">
-                <div className="shop-wrapper">
-                    <div className="shop-nav">
-                        <h5>SHOP //</h5>
-                        <h2>All Products</h2>
-                        <ul>
-                            {categories?.map((category) => {
-                                return (
-                                    <li key={category.id}>{category.name}</li>
-                                );
-                            })}
-                        </ul>
-                    </div>
-                    <div className="shop-products">
-                        {allProducts?.map((item, i) => {
-                            return (
-                                <div key={i} className="product">
-                                    <div className="product-box">
-                                        <img
-                                            src={require(`../images/${item.image}`)}
-                                        ></img>
-                                    </div>
-                                    <div className="product-description">
-                                        <h3>{item.name}</h3>
-                                        <p>{`C$${item.price}`}</p>
-                                    </div>
-                                </div>
-                            );
+    return (
+        <div className="shop-container">
+            <div className="shop-wrapper">
+                <div className="shop-nav">
+                    <h5>SHOP //</h5>
+                    <h2>All Products</h2>
+                    <ul>
+                        {categories?.map((category) => {
+                            return <li key={category.id}>{category.name}</li>;
                         })}
-                    </div>
+                    </ul>
+                </div>
+                <div className="shop-products">
+                    {allProducts?.map((item, i) => {
+                        return (
+                            <div key={i} className="product">
+                                <div className="product-box">
+                                    <img
+                                        src={require(`../images/${item.image}`)}
+                                    ></img>
+                                </div>
+                                <div className="product-description">
+                                    <h3>{item.name}</h3>
+                                    <p>{`C$${item.price}`}</p>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Shop;
