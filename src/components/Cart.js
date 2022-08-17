3a085725612d09d5547e0b1f739556d337028e14
Ben Tucker <bwtucker4@gmail.com>
2022-08-16 19:43:19 -0700

    Finish first build of cart and link to display/hide buttons

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,15 +5,39 @@ import categories from "../products/categories";
 import "../styles/Cart.css";
 
 const Cart = (props) => {
+    function hideCart() {
+        document.querySelector(".cart").style.display = "none";
+    }
+
     return (
         <div className="cart">
             <div className="cart-backdrop"></div>
             <div className="cart-container">
                 <img
+                    onClick={hideCart}
                     className="close-cart"
                     src={require("../images/close.png")}
                 ></img>
                 <h2 className="cart-title">Your Shopping Cart</h2>
+                <div className="cart-item">
+                    <div className="cart-item-image">
+                        <img
+                            src={require("../images/taylormade-driver-1.png")}
+                        ></img>
+                    </div>
+                    <div className="cart-item-details">
+                        <div className="cart-item-name">
+                            TaylorMade Stealth Driver
+                        </div>
+                        <div className="cart-item-price">C$749.99</div>
+                        <div className="cart-quantity-bin">
+                            <img src={require("../images/minus.png")}></img>
+                            <div className="cart-quantity-counter">1</div>
+                            <img src={require("../images/plus.png")}></img>
+                        </div>
+                    </div>
+                </div>
+                <button>Checkout</button>
             </div>
         </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,16 @@ class Header extends Component {
         super(props);
     }
 
+    showCart = () => {
+        document.querySelector(".cart").style.display = "flex";
+    };
+
     render() {
         return (
             <header>
                 <div className="app-title">
                     <Link to="/">
-                        <div onClick={this.props.changeColor}>THE RANGE</div>
+                        <div>THE RANGE</div>
                     </Link>
                 </div>
                 <ul>
@@ -30,19 +34,17 @@ class Header extends Component {
                             </svg>
                         </Link>
                     </li>
-                    <li>
-                        <Link to="/cart">
-                            <svg
-                                id="cart-icon"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="white"
-                            >
-                                <path d="M16 6v-2c0-2.209-1.791-4-4-4s-4 1.791-4 4v2h-5v18h18v-18h-5zm-7-2c0-1.654 1.346-3 3-3s3 1.346 3 3v2h-6v-2zm10 18h-14v-14h3v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h6v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h3v14z" />
-                            </svg>
-                        </Link>
+                    <li onClick={this.showCart}>
+                        <svg
+                            id="cart-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="white"
+                        >
+                            <path d="M16 6v-2c0-2.209-1.791-4-4-4s-4 1.791-4 4v2h-5v18h18v-18h-5zm-7-2c0-1.654 1.346-3 3-3s3 1.346 3 3v2h-6v-2zm10 18h-14v-14h3v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h6v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h3v14z" />
+                        </svg>
                     </li>
                 </ul>
             </header>
