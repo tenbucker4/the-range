fe59e816d6637b33f95a6a85ec1e81436cfafaf9
Ben Tucker <bwtucker4@gmail.com>
2022-08-25 11:59:51 -0700

    Added basename to browserrouter

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -130,7 +130,7 @@ class RouteSwitch extends Component {
 
     render() {
         return (
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route
                         exact
