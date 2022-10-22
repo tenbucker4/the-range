d1b33d6f6a5fce1e0675bd8a8a965904380665cd
Ben Tucker <bwtucker4@gmail.com>
2022-10-21 17:48:11 -0700

    Begin refactoring project starting with header. Improving responsiveness and spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,28 +10,33 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <div className="app-title">
-                    <Link to="/the-range">
-                        <div>THE RANGE</div>
-                    </Link>
-                </div>
-                <ul>
-                    <li onClick={this.props.showCart}>
-                        <div className="cart-counter">
-                            {this.props.cart.length}
-                        </div>
-                        <svg
-                            id="cart-icon"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="white"
+                <div className="header-container">
+                    <div className="app-title">
+                        <Link to="/the-range">
+                            <div>THE RANGE</div>
+                        </Link>
+                    </div>
+                    <div className="cart-icon">
+                        <div
+                            className="cart-icon-pair"
+                            onClick={this.props.showCart}
                         >
-                            <path d="M16 6v-2c0-2.209-1.791-4-4-4s-4 1.791-4 4v2h-5v18h18v-18h-5zm-7-2c0-1.654 1.346-3 3-3s3 1.346 3 3v2h-6v-2zm10 18h-14v-14h3v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h6v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h3v14z" />
-                        </svg>
-                    </li>
-                </ul>
+                            <svg
+                                id="cart-icon"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="white"
+                            >
+                                <path d="M16 6v-2c0-2.209-1.791-4-4-4s-4 1.791-4 4v2h-5v18h18v-18h-5zm-7-2c0-1.654 1.346-3 3-3s3 1.346 3 3v2h-6v-2zm10 18h-14v-14h3v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h6v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h3v14z" />
+                            </svg>
+                            <div className="cart-counter">
+                                {this.props.cart.length}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </header>
         );
     }
